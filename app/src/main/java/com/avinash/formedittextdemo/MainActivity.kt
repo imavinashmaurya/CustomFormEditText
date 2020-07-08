8fa6822747d9b242e5fcbd54fb6e270740bebc9d
Avinash <avinash@infiniteit.biz>
2020-07-08 18:18:07 +0530

    first commit

---
+++ app/src/main/java/com/avinash/formedittextdemo/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.avinash.formedittextdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        etForm?.setHint("Hint123")
+        etForm1?.setHint("Hint@123")
+        etForm?.isEnabled = false
+
+        btSuccess?.setOnClickListener {
+            etForm?.showCompleteStatus()
+
+        }
+        btError?.setOnClickListener {
+            etForm?.setError("error123")
+
+        }
+        btProgress?.setOnClickListener {
+            etForm?.enableProgress()
+        }
+
+        btSuccess1?.setOnClickListener {
+            etForm1?.showCompleteStatus()
+
+        }
+        btError1?.setOnClickListener {
+            etForm1?.setError("error@123")
+
+        }
+        btProgress1?.setOnClickListener {
+            etForm1?.enableProgress()
+        }
+
+
+        btHideSuccess?.setOnClickListener {
+            etForm?.clearCompleteStatus()
+
+        }
+        btHideError?.setOnClickListener {
+            etForm?.clearError()
+
+        }
+        btHideProgress?.setOnClickListener {
+            etForm?.clearProgress()
+        }
+
+        btHideSuccess1?.setOnClickListener {
+            etForm1?.clearCompleteStatus()
+
+        }
+        btHideError1?.setOnClickListener {
+            etForm1?.clearError()
+
+        }
+        btHideProgress1?.setOnClickListener {
+            etForm1?.clearProgress()
+        }
+
+        btAction?.setOnClickListener {
+            etForm?.setActionIcon(R.drawable.ic_custom_action)
+        }
+        btAction1?.setOnClickListener {
+            etForm1?.setActionIcon(R.drawable.ic_custom_action)
+        }
+    }
+}
---
+++ formEditTextView/src/main/java/com/avinash/formedittextview/CustomFormEditText.kt
@@ -0,0 +1,121 @@
+package com.avinash.formedittextview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.textfield.TextInputLayout
+
+
+class CustomFormEditText : ConstraintLayout {
+
+    private var mRootView: View? = null
+    private var mInputTextLayout: TextInputLayout? = null
+    private var mInputTextEditText: EditText? = null
+    private var mCardView: CardView? = null
+    private var mTvError: TextView? = null
+    private var mProgress: ProgressBar? = null
+    private var mIvAction: ImageView? = null
+    private var mllError: LinearLayout? = null
+    private var hint: String? = null
+    private var mContext: Context? = null
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        initUI(context)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet?) {
+        initUI(context)
+        //read xml attributes
+
+        //read xml attributes
+        val ta =
+            context.theme.obtainStyledAttributes(attrs, R.styleable.CustomFormEditText, 0, 0)
+
+        val hintAppearance: Int =
+            ta.getResourceId(R.styleable.CustomFormEditText_customHintTextStyle, -1)
+        if (hintAppearance != -1) {
+            setHintStyle(hintAppearance)
+        }
+    }
+
+    fun setHintStyle(hintAppearance: Int) {
+        mInputTextLayout?.setHintTextAppearance(hintAppearance)
+    }
+
+
+    private fun initUI(context: Context) {
+        mContext = context
+        val inflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        mRootView = inflater.inflate(R.layout.custom_form_edit_text, this)
+        mCardView = mRootView?.findViewById(R.id.cardView)
+        mInputTextLayout = mRootView?.findViewById(R.id.tilForm)
+        mInputTextEditText = mRootView?.findViewById(R.id.editText)
+        mIvAction = mRootView?.findViewById(R.id.ivAction)
+        mProgress = mRootView?.findViewById(R.id.pbForm)
+        mllError = mRootView?.findViewById(R.id.llError)
+        mTvError = mRootView?.findViewById(R.id.tvError)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    fun setError(error: String) {
+        mllError?.visibility = View.VISIBLE
+        mTvError?.text = error
+    }
+
+    fun clearError() {
+        mllError?.visibility = View.GONE
+        mTvError?.text = ""
+    }
+
+    fun showCompleteStatus() {
+        mProgress?.visibility = View.GONE
+        mIvAction?.visibility = View.VISIBLE
+    }
+
+    fun clearCompleteStatus() {
+        mIvAction?.visibility = View.GONE
+    }
+
+    fun setActionIcon(icon: Int) {
+        mIvAction?.setImageResource(icon)
+        showCompleteStatus()
+    }
+
+    fun enableProgress() {
+        clearCompleteStatus()
+        mProgress?.visibility = View.VISIBLE
+    }
+
+    fun clearProgress() {
+        mProgress?.visibility = View.GONE
+    }
+
+    fun setHint(hint: String) {
+        mInputTextLayout?.hint = hint
+    }
+
+}
